24c8f83dce9347b805b1a4344e09fa63d596023e
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-08 10:14:05 -0700

    Moved height and width to container instead of face of card

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,9 +1,9 @@
 export default function Card() {
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <div className="relative card-container">
+      <div className="relative card-container w-[180px] h-[280px]">
         <div className="backface-hidden"></div>
-        <div className="backface-hidden flex items-center justify-center bg-amber-950 rounded-2xl w-[180px] h-[280px]">
+        <div className="flex items-center justify-center w-full h-full backface-hidden bg-amber-950 rounded-2xl">
           <div className="w-[95%] relative h-[97%] rounded-xl border-4 border-amber-500">
             <div className="absolute w-[10%] bg-amber-950 border-b-4 border-r-4 border-amber-500 rounded-br-full top-[-8px] left-[-8px] aspect-square "></div>
             <div className="absolute w-[10%] border-b-4 border-l-4 border-amber-500 rounded-bl-full aspect-square top-[-8px] right-[-8px] bg-amber-950"></div>
